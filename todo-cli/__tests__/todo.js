6d228653a2cd96b33262f6a36d72747ee2dee4db
Deepa200416 <dinnu2000dinnu@gmail.com>
2023-12-13 19:08:27 +0530

    list of tests are added

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -15,11 +15,11 @@ describe("Todolist Test Suite", () => {
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString().split("T")[0], // Assuming the dueDate is set to today
+      dueDate: new Date().toISOString().split("T")[0],
     });
   });
 
-  test("Should add new todo", () => {
+  test("Should add new todo ", () => {
     const todoItemsCount = all.length;
     add({
       title: "Test todo",
@@ -29,7 +29,7 @@ describe("Todolist Test Suite", () => {
     expect(all.length).toBe(todoItemsCount + 1);
   });
 
-  test("Should mark a todo as complete", () => {
+  test("Should mark a todo as complete (Intentional Failure)", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
@@ -37,23 +37,22 @@ describe("Todolist Test Suite", () => {
 
   test("Should retrieve overdue todos", () => {
     const overdueTodos = overdue();
-    expect(overdueTodos.length).toBe(0); // Modify based on your test data
+    expect(overdueTodos.length).toBe(0);
   });
 
   test("Should retrieve due today todos", () => {
     const dueTodayTodos = dueToday();
-    expect(dueTodayTodos.length).toBeGreaterThanOrEqual(0); // Modify based on your test data
+    expect(dueTodayTodos.length).toBeGreaterThanOrEqual(0);
   });
 
   test("Should retrieve due later todos", () => {
     const dueLaterTodos = dueLater();
-    expect(dueLaterTodos.length).toBeGreaterThanOrEqual(0); // Modify based on your test data
+    expect(dueLaterTodos.length).toBeGreaterThanOrEqual(0);
   });
 
-  test("Should format todos for display", () => {
+  test("Should format todos for display (Intentional Failure)", () => {
     const today = new Date().toISOString().split("T")[0];
     const displayableList = toDisplayableList(all, today);
-    // You can add more specific expectations based on your data and logic
-    expect(displayableList).toContain("[ ] Test todo");
+    expect(displayableList).toContain("[x] Test todo");
   });
 });
